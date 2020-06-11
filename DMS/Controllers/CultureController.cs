603cc564c1cc0da42598b5f215555fa06c234bbc
osayed <osayedzahida@gmail.com>
2020-06-11 11:43:23 +0300

    Added

--- DMS/Controllers/CultureController.cs
+++ DMS/Controllers/CultureController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DevExtreme.AspNet.Data.ResponseModel;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DMS.Controllers
 {
+    [AllowAnonymous]
     [Route("api/[controller]")]
     public class CultureController : Controller
     {
